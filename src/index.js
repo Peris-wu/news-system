899745b1008a0a9ecddaaf81af71de7c1ad10d49
peris <964480899@qq.com>
2022-05-31 23:26:37 +0800

    登录功能处理完毕

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import store from './redux'
+import { Provider } from 'react-redux'
+// import { persistStore } from 'redux-persist'
+// import { PersistGate } from 'redux-persist/integration/react'
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+// const persistor = persistStore(store)
+root.render(
+  <Provider store={store}>
+    {/* <PersistGate persistor={persistor}>
+    </PersistGate> */}
+    <App />
+  </Provider>
+)
--- src/pages/NewsSandBox/TopHeader/WelcomeUser/index.jsx
+++ src/pages/NewsSandBox/TopHeader/WelcomeUser/index.jsx
@@ -1,29 +1,42 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import { UserOutlined } from '@ant-design/icons';
 import { Avatar } from 'antd';
 import { Dropdown, Menu } from 'antd';
 import welcomeStyle from './index.module.scss'
+import handlerStorage from '../../../../utils/handlerLocalStorage'
 
-const menu = (
-  <Menu
-    onClick={() => { console.log(1); }}
-    items={[
-      {
-        key: 'super-manager',
-        label: '超级管理员'
-      },
-      {
-        key: 'login-out',
-        label: '退出',
-        danger: true
-      }
-    ]}
-  />
-)
+
 export default function WelcomUser () {
+  let { username, role: { roleName } } = handlerStorage.getStorage()
+  const history = useHistory()
+  const menu = (
+    <Menu
+      onClick={(item) => {
+        if (item.key === 'login-out') {
+          handlerStorage.removeStorage()
+          history.replace('/login')
+        }
+      }}
+      items={[
+        {
+          key: 'roleType',
+          label: roleName
+        },
+        {
+          key: 'login-out',
+          label: '退出',
+          danger: true
+        }
+      ]}
+    />
+  )
   return (
     <div className={welcomeStyle['welcome-user-wrap']}>
-      <div className={welcomeStyle['welcome-user-wrap']}>WelcomUser</div>
+      <div className={welcomeStyle['welcome-user-wrap']}>
+        Welcom
+        <span style={{ color: 'blue', paddingLeft: '5px' }}>{username}</span>
+      </div>
       <Dropdown overlay={menu} arrow={true}>
         <Avatar size={48} icon={<UserOutlined />} />
       </Dropdown>
